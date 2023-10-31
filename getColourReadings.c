338eeee918671018bb55d03a40c442169d920c66
pqienso <85116669+pqienso@users.noreply.github.com>
2023-10-31 11:28:13 +0800

    Update getColourReadings.c

--- getColourReadings.c
+++ getColourReadings.c
@@ -1,17 +1,60 @@
+#include <MeMCore.h>
+#define LIGHTSENSOR A2
+#define ULTRASONIC 12
+#define LDR A3
+#define S1 A0
+#define S2 A1
+
+#define TIMEOUT 1000 // Max microseconds to wait; choose according to max distance of wall
+#define SPEED_OF_SOUND 340 // Update according to your own experiment
+#define COLOURSENSORCOOLDOWN 50 // timeout in ms for coloursensor
+
+float coloursArray[6][3] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+char colourStr[6][7] = {"Red", "Green", "Blue", "Orange", "Purple", "White"};
+
+int detectColour()
+{
+// Shine each colour, read LDR after some delay
+  int readColour[3];
+  for(int i = 0; i < 3; i++)
+  {
+    decoder(i+1);
+    delay(COLOURSENSORCOOLDOWN);
+    readColour[i] = analogRead(LDR);
+  }
+// Run algorithm for colour decoding
+  int smallestError = 2147483647, colour = 0;
+  for(int i = 0; i < 6; i++)
+  {
+    int sumSquareError = 0;
+    for(int j = 0; j < 3; j++)
+    {
+      sumSquareError += (readColour[j] - coloursArray[i][j]) * (readColour[j] - coloursArray[i][j]);
+    }
+    if (sumSquareError < smallestError)
+    {
+      colour = i;
+      smallestError = sumSquareError;
+    }
+  }
+  return colour;
+}
+
 void getColourReadings(int scansPerColour){
   for(int i = 0; i < 6; i++)
   {
+    decoder(0);
     Serial.println("Colour calibrating:");
     Serial.println(colourStr[i]);
     delay(5000);     //delay for five seconds for getting sample ready
-    Serial.println("Scanning...")
+    Serial.println("Scanning...");
     for(int j = 0; j < scansPerColour; j++)
     {
       for(int k = 1; k <= 3; k++)
       {
         decoder(k);
         delay(50);
-        coloursArray[i][k - 1] += analogRead(A3);
+        coloursArray[i][k - 1] += analogRead(LDR);
       }
     }
     for(int k = 0; k < 3; k++)
@@ -21,22 +64,6 @@ void getColourReadings(int scansPerColour){
     Serial.println("Colour scanned.");
     delay(2000);
   }
-
-  void decoder(int mode) { //0 for IR, 1 for Red, 2 for Green, 3 for Blue
-  if (mode == 0) {
-    digitalWrite(S1, LOW);
-    digitalWrite(S2, LOW);
-  } else if (mode == 1) {
-    digitalWrite(S1, HIGH);
-    digitalWrite(S2, HIGH);
-  } else if (mode == 2) {
-    digitalWrite(S1, HIGH);
-    digitalWrite(S2, LOW);
-  } else {
-    digitalWrite(S1, LOW);
-    digitalWrite(S2, HIGH);
-  }
-}
 
   Serial.println("All colours scanned.");
   for(int i = 0; i < 6; i++)
@@ -53,4 +80,34 @@ void getColourReadings(int scansPerColour){
     Serial.print(")");
     Serial.println(" ");
   }
+  delay(10000);
+}
+
+void decoder(int mode)
+{ //0 for IR, 1 for Red, 2 for Green, 3 for Blue
+  if (mode == 0) {
+    digitalWrite(S1, LOW);
+    digitalWrite(S2, LOW);
+  } else if (mode == 1) {
+    digitalWrite(S1, HIGH);
+    digitalWrite(S2, HIGH);
+  } else if (mode == 2) {
+    digitalWrite(S1, HIGH);
+    digitalWrite(S2, LOW);
+  } else {
+    digitalWrite(S1, LOW);
+    digitalWrite(S2, HIGH);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  getColourReadings(10);
+}
+
+void loop(){
+ delay(1000);
+ Serial.println("colour detected:");
+ Serial.println(colourStr[detectColour()]);
+ Serial.println("---------");
 }
