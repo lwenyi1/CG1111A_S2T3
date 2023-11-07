387e9a8a321b1c5c5c0748d03fee26524742b881
pqienso <85116669+pqienso@users.noreply.github.com>
2023-11-07 12:37:02 +0800

    Update maincode.c

--- maincode.c
+++ maincode.c
@@ -5,6 +5,10 @@
 #define S1 A0
 #define S2 A1
 #define ULTRASONIC 12
+MeBuzzer buzzer;
+MeLineFollower lineFinder(PORT_2); // assigning lineFinder to RJ25 port 2
+MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
+MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
 
 #define TIMEOUT 5000 
 #define SPEED_OF_SOUND 340 
@@ -13,22 +17,18 @@
 #define NINETYDEG 640
 #define IRCUTOFF 300
 #define CONSEC_TURN_WAIT_TIME 850
+const uint8_t motorSpeed = 255;// Setting motor speed to an integer between 1 and 255
+const uint8_t turningSpeed = 170;
 
-MeBuzzer buzzer;
-MeLineFollower lineFinder(PORT_2); // assigning lineFinder to RJ25 port 2
-
-MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
-MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
-uint8_t motorSpeed = 255;// Setting motor speed to an integer between 1 and 255
-uint8_t turningSpeed = 170;
-
+//calibrated colour values
 float coloursArray[6][3] = {{241.20, 449.00, 224.60}, {126.10, 540.00, 229.50}, {150.90, 603.20, 417.70}, {255.80, 533.60, 255.60}, {162.10, 528.40, 347.00}, {272.20, 685.10, 472.30}};
-char colourStr[6][7] = {"Red", "Green", "Blue", "Orange", "Purple", "White"};
 
-void celebrate() {
+void celebrate() 
+{
   int notes[] = {784, 0, 784, 0, 880, 0, 1046, 0, 988, 0, 1175, 1319, 0, 1046};
   int rhythm[] = {18, 2, 18, 2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 60};
-  for (int i = 0; i < 14; i++) {
+  for (int i = 0; i < 14; i++)
+  {
     if(notes[i] == 0)
     {
       buzzer.noTone();
@@ -41,27 +41,33 @@ void celebrate() {
 
 void decoder(int mode)
 { //0 for IR, 1 for Red, 2 for Green, 3 for Blue
-  if (mode == 0) {
+  if (mode == 0)
+  {
     digitalWrite(S1, LOW);
     digitalWrite(S2, LOW);
   }
-  else if (mode == 1) {
+  else if (mode == 1)
+  {
     digitalWrite(S1, HIGH);
     digitalWrite(S2, HIGH);
-  } else if (mode == 2) {
+  }
+  else if (mode == 2)
+  {
     digitalWrite(S1, HIGH);
     digitalWrite(S2, LOW);
-  } else {
+  }
+  else
+  {
     digitalWrite(S1, LOW);
     digitalWrite(S2, HIGH);
   }
 }
 
-double read_ultrasonic() {
+double read_ultrasonic()
+{
   pinMode(ULTRASONIC, OUTPUT);
   digitalWrite(ULTRASONIC, LOW);
   delayMicroseconds(2);
-  
   digitalWrite(ULTRASONIC, HIGH);
   delayMicroseconds(10);
   digitalWrite(ULTRASONIC, LOW);
@@ -71,17 +77,16 @@ double read_ultrasonic() {
   return distance;
 }
 
-bool read_IR_sensor() {
+bool read_IR_sensor()
+{
+  decoder(3);
   long baseline = analogRead(IR_SENSOR);
   decoder(0);
   delay(10);
   long feedback = analogRead(IR_SENSOR);
-  if (baseline - feedback > IRCUTOFF) {
-    decoder(3);
-    return true;
-  }
   decoder(3);
-  return false;
+  if (baseline - feedback > IRCUTOFF) return true;
+  else return false;
 }
 
 int detectColour()
@@ -114,11 +119,11 @@ int detectColour()
 
 void stopMotor()
 {
-  leftMotor.stop(); // Stop left motor
-  rightMotor.stop(); // Stop right motor
+  leftMotor.stop();
+  rightMotor.stop();
 }
 
-void moveForward() 
+void moveForward()
 {
   leftMotor.run(-motorSpeed);
   rightMotor.run(motorSpeed);
@@ -159,7 +164,7 @@ void setup()
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   decoder(2);
-  delay(3000);
+  delay(2000);
   decoder(3);
 }
 
