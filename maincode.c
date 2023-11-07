2ce52332aff648f5226c74fef3bf7c11ace01fc4
Liu-Xuyan <142197805+Liu-Xuyan@users.noreply.github.com>
2023-11-07 10:33:41 +0800

    Update maincode.c

--- maincode.c
+++ maincode.c
@@ -1,87 +1,34 @@
-#include <MeMCore.h>
-#define LIGHTSENSOR A2
+#include "MeMCore.h"
+#define TIMEOUT 5000 
+#define SPEED_OF_SOUND 340 
 #define ULTRASONIC 12
-#define LDR A3
+#define safe_distance 8
+
+#define LDR A2
+#define IR_SENSOR A3
+
 #define S1 A0
 #define S2 A1
 
-#define TIMEOUT 1000 // Max microseconds to wait; choose according to max distance of wall
-#define SPEED_OF_SOUND 340 // Update according to your own experiment
-#define COLOURSENSORCOOLDOWN 50 // timeout in ms for coloursensor
-#define NINETY 230 // time taken to turn 90 deg
+#define COLOURSENSORCOOLDOWN 500
+
+MeBuzzer buzzer;
+MeLineFollower lineFinder(PORT_2); // assigning lineFinder to RJ25 port 2
 
 MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
 MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
-MeLineFollower lineFinder(PORT_2); // assigning lineFinder to RJ25 port 2
-uint8_t motorSpeed = 255;
+uint8_t motorSpeed = 255;// Setting motor speed to an integer between 1 and 255
+uint8_t turningSpeed = 170;
 
-int closeToLeft = 0;
-int closeToRight = 0;
-float coloursArray[6][3] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+float coloursArray[6][3] = {{369.8,523.8,347.6}, {263.1,588.4,339.9}, {269.6,637.2,476.7}, {380.5,588.5,374.9}, {279.0,574,422.1}, {410,715.4,546.5}};
+char colourStr[6][7] = {"Red", "Green", "Blue", "Orange", "Purple", "White"};
 
-void celebrate() 
-{// Code for playing celebratory tune
-}
-void stopMotor()
-{
-  leftMotor.stop(); // Stop left motor
-  rightMotor.stop(); // Stop right motor
-}
-void go_straight() 
-{
-  leftMotor.run(255);
-  rightMotor.run(-255);
-}
-
-void left_turn() {
-  leftMotor.run(255);
-  rightMotor.run(255);
-  delay(NINETY);
-  go_straight() ;
-}
-
-void right_turn() {
-  rightMotor.run(-255);
-  leftMotor.run(-255);
-  delay(NINETY+10);
-  go_straight() ;
-}
-
-void u_turn() {
-  leftMotor.run(-255);
-  rightMotor.run(-255);
-  delay(3.7 * NINETY);
-  go_straight() ;
-}
-
-void consec_right() {
-  right_turn();
-  go_straight() ;
-  delay(1000); //adjust time needed to go straight
-  right_turn();
-  go_straight() ;
-}
-
-void consec_left() {
-  left_turn();
-  go_straight();
-  delay(1000);
-  left_turn();
-  go_straight() ;
-}
-
-void nudgeLeft() 
-{
-  // Code for nudging slightly to the left for some short interval
-  stopMotor();
-  rightMotor.run(motorSpeed);
-  delay(50);
-}
-void nudgeRight() 
-{// Code for nudging slightly to the right for some short interval
-  stopMotor();
-  leftMotor.run(-motorSpeed);
-  delay(50);
+void setup() {
+pinMode(S1, OUTPUT);
+pinMode(S2, OUTPUT);
+decoder(2);
+delay(3000);
+decoder(3);
 }
 
 void decoder(int mode)
@@ -89,7 +36,8 @@ void decoder(int mode)
   if (mode == 0) {
     digitalWrite(S1, LOW);
     digitalWrite(S2, LOW);
-  } else if (mode == 1) {
+  }
+  else if (mode == 1) {
     digitalWrite(S1, HIGH);
     digitalWrite(S2, HIGH);
   } else if (mode == 2) {
@@ -101,22 +49,47 @@ void decoder(int mode)
   }
 }
 
+double read_ultrasonic() {
+  pinMode(ULTRASONIC, OUTPUT);
+  digitalWrite(ULTRASONIC, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(ULTRASONIC, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC, LOW);
+  pinMode(ULTRASONIC, INPUT);
+  long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
+  double distance = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
+  return distance;
+}
+
+int read_IR_sensor() {
+  long baseline = analogRead(IR_SENSOR);
+  decoder(0);
+  delay(10);
+  long feedback = analogRead(IR_SENSOR);
+  if (baseline - feedback > 250) {
+    decoder(3);
+    return 1;
+  }
+  decoder(3);
+  return 0;
+}
 
 int detectColour()
 {
 // Shine each colour, read LDR after some delay
-  int readColour[3];
+  float readColour[3];
   for(int i = 0; i < 3; i++)
   {
     decoder(i+1);
     delay(COLOURSENSORCOOLDOWN);
-    readcolour[i] = analogRead(LDR);
+    readColour[i] = analogRead(LDR);
   }
-// Run algorithm for colour decoding
-  int smallestError = 2147483647, colour = 0;
+  float smallestError = 1470000, colour = 2;
   for(int i = 0; i < 6; i++)
   {
-    int sumSquareError = 0;
+    float sumSquareError = 0;
     for(int j = 0; j < 3; j++)
     {
       sumSquareError += (readColour[j] - coloursArray[i][j]) * (readColour[j] - coloursArray[i][j]);
@@ -130,95 +103,88 @@ int detectColour()
   return colour;
 }
 
-void setup()
-{
-// Configure pinMode for A0, A1, A2, A3
-  Serial.begin(9600);
+void turn_left(){
+    leftMotor.run(turningSpeed);
+    rightMotor.run(turningSpeed);
+    delay(640);
+    leftMotor.run(0);
+    rightMotor.run(0);
+    delay(500);
 }
-void loop()
-{
-  
 
-// Read ultrasonic sensing distance (choose an appropriate timeout)
-  pinMode(ULTRASONIC, OUTPUT);
-  digitalWrite(ULTRASONIC, LOW);
-  delayMicroseconds(2);
-  digitalWrite(ULTRASONIC, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(ULTRASONIC, LOW);
-  pinMode(ULTRASONIC, INPUT);
-  long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
-  int dist_cm = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
-  Serial.println(closeToLeft);
-  if (dist_cm < 6 && dist_cm != 0) 
-  {
-    closeToLeft++;
-  } 
+void turn_right() {
+    leftMotor.run(-turningSpeed);
+    rightMotor.run(-turningSpeed);
+    delay(640);
+    leftMotor.run(0);
+    rightMotor.run(0);
+    delay(500);
+}
 
-// Read IR sensing distance (turn off IR, read IR detector, turn on IR, read IR detector, estimate distance)
-  decoder(1);
-  int IRBaseline = analogread(LIGHTSENSOR);
-  decoder(0);
-  if(analogRead(LIGHTSENSOR) - IRBaseLine > 100)
-  {
-    closeToRight++;
-  }
+void turn_around() {
+    leftMotor.run(turningSpeed);
+    rightMotor.run(turningSpeed);
+    delay(1280);
+    leftMotor.run(0);
+    rightMotor.run(0);
+    delay(500);
+}
 
-// if within black line, stop motor, detect colour, and take corresponding action
+int colour = 0;
 
-  
-// else if too near to left wall, nudge right
-  if(closeToLeft >= 8)
-  {
-    closeToLeft = 0;
-    Serial.println("nudge right");
-    nudgeRight();
-  }
-// else if too near to right wall, nudge left
-  else if(closeToRight >= 8)
-  {
-    closeToRight = 0;
-    nudgeLeft();
-  }
-
-
-  else if(lineFinder.readSensors() == S1_IN_S2_IN)
-  {
-    stopMotor();
-    int colour = detectColour();
-    int red = 0, green = 1, blue = 2, orange = 3, purple = 4;
-    if (colour == red)
-    {
-      leftTurn() ;
+void loop() {
+  if (colour != 5) {
+  double distance = read_ultrasonic();//read the distance from ultranosic sensor
+  int IR_bool = read_IR_sensor();
+  int sensorState = lineFinder.readSensors(); // read the line sensor's state
+  if (sensorState == S1_IN_S2_IN) {
+    leftMotor.run(0); // Left wheel halt
+    rightMotor.run(0); // Right wheel halt
+    colour = detectColour();
+    if(colour == 0) {
+      turn_left();
     }
-    else if (colour == green)
-    {
-      turnRight() ;
+    if(colour == 1) {
+      turn_right();
     }
-    else if (colour == blue)
-    {
-      doubleRightTurn() ;
+    if(colour == 3) {
+      turn_around();
     }
-    else if (colour == orange)
-    {
-      uTurn() ;
+    if(colour == 4) {
+      turn_left();
+      leftMotor.run(-motorSpeed);
+      rightMotor.run(motorSpeed);
+      delay(900);
+      turn_left();
     }
-    else if (colour == purple)
-    {
-      doubleLeftTurn() ;
+    if(colour == 2) {
+      turn_right();
+      leftMotor.run(-motorSpeed);
+      rightMotor.run(motorSpeed);
+      delay(900);
+      turn_right();
     }
-    else 
-    {
-      celebrate() ;
-      delay( 10000 ) ;
-    }
-  }
-// else move forward
-  else
-  {
-    //go forward
+  } else if (distance < safe_distance && distance > 0) {
+    leftMotor.run(-motorSpeed);
+    rightMotor.run(0);
+    delay(2); // Adjust this delay as needed
+  } else if (IR_bool == 1) {
+    leftMotor.run(0);
+    rightMotor.run(motorSpeed);
+  } else {
     leftMotor.run(-motorSpeed);
     rightMotor.run(motorSpeed);
   }
-  
+  } else {
+      leftMotor.run(0);
+      rightMotor.run(0);
+      buzzer.tone(392, 200);
+    buzzer.tone(523, 200);
+    buzzer.tone(659, 200);
+    buzzer.tone(784, 200);
+    buzzer.tone(659, 150);
+    buzzer.tone(784, 400);
+    buzzer.noTone();
+    delay(5000);
+  }
 }
