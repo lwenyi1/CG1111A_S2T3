0fdaeb717e5229defa9580bdd4484bd3dfa2fe63
pqienso <85116669+pqienso@users.noreply.github.com>
2023-11-09 11:28:23 +0800

    Update B03_S2_T3_maincode.c

--- B03_S2_T3_maincode.c
+++ B03_S2_T3_maincode.c
@@ -1,5 +1,13 @@
+/**
+ * A-maze-ing Race Mbot Arduino Code
+ * 
+ * @file B03_S2_T3_maincode.c
+ * @author B03_S2_T3
+ */
+
 #include "MeMCore.h"
 
+//define pins and components
 #define LDR A2
 #define IR_SENSOR A3
 #define S1 A0
@@ -10,6 +18,7 @@ MeLineFollower lineFinder(PORT_2);
 MeDCMotor leftMotor(M1);
 MeDCMotor rightMotor(M2);
 
+//define coloured paper ID list
 #define RED 0
 #define GREEN 1
 #define BLUE 2
@@ -17,16 +26,17 @@ MeDCMotor rightMotor(M2);
 #define PURPLE 4
 #define WHITE 5
 
+//define timings, cutoff values and speed
 #define TIMEOUT 5000 
 #define SPEED_OF_SOUND 340 
 #define SAFEDISTANCE 9
-#define COLOURSENSORCOOLDOWN 50
-#define NINETYDEG 360
-#define IRCUTOFF 290
-#define CONSEC_TURN_WAIT_TIME 840
+#define COLOURSENSORCOOLDOWN 500
+#define NINETYDEG 640
+#define IRCUTOFF 300
+#define CONSEC_TURN_WAIT_TIME 850
 #define NUDGETIME 2
 const uint8_t motorSpeed = 255;
-const uint8_t turningSpeed = 255;
+const uint8_t turningSpeed = 170;
 
 //calibrated colour values
 float coloursArray[8][3] = 
@@ -34,35 +44,37 @@ float coloursArray[8][3] =
 {296.80, 557.50, 323.20}, {216.00, 562.40, 414.80}, {328.40, 694.80, 528.10}, //orange, purple, white
 {181.70, 494.70, 300.70}, {328.40, 694.80, 528.10}}; //min, max values for normalisation
 
+/**
+ * Plays a celebratory tune using the Mbot buzzer. Runs through two arrays, one
+ * containing frequencies for the notes and one containing timings for the
+ * rhythm
+ */
 void celebrate() 
 {
-  int notes[] = 
-  {392, 523, 659, 784, 1046, 1319, 1568, 1319, 0,
-  415, 523, 622, 831, 1046, 1245, 1661, 1245, 0, 
-  466, 587, 698, 932, 1175, 1397, 1865, 0,
-  1865, 0, 1865, 0, 1865, 0, 2093};
-
-  int rhythm[] =
-  {20, 20, 20, 20, 20, 20, 60, 40, 20,
-  20, 20, 20, 20, 20, 20, 60, 40, 20,
-  20, 20, 20, 20, 20, 20, 40, 20,
-  10, 10, 10, 10, 10, 10, 120
-  };
-
-  for (int i = 0; i < 33; i++)
+  int notes[] = {784, 0, 784, 0, 880, 0, 1046, 0, 988, 0, 1175, 1319, 0, 1046};
+  int rhythm[] = {18, 2, 18, 2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 60};
+  for (int i = 0; i < 14; i++) 
   {
-    if(notes[i] == 0)
-    {
+    if(notes[i] == 0) {
       buzzer.noTone();
-      delay(rhythm[i] * 8);
+      delay(rhythm[i] * 12);
     }
-    else buzzer.tone(notes[i], rhythm[i] * 8);
+    else buzzer.tone(notes[i], rhythm[i] * 12);
   }
   buzzer.noTone();
 }
 
-void decoder(int mode)
-{ //0 for IR, 1 for Red, 2 for Green, 3 for Blue
+/**
+ * Writes the pins connected to the 2-4 decoder to HIGH or LOW depending on
+ * the input.
+ *
+ * @param[in] mode An integer value corresponding to the desired decoder
+ *                 output. 0 turns on the IR, 1 turns on the Red LED, 2 turns
+ *                 on the Green LED, 3 turns on the Blue LED.
+ * @param[out] S1 Pin for decoder's S1 input, set to HIGH or LOW.
+ * @param[out] S2 Pin for decoder's S2 input, set to HIGH or LOW.
+ */
+void decoder(int mode) {
   if (mode == 0)
   {
     digitalWrite(S1, LOW);
@@ -85,7 +97,19 @@ void decoder(int mode)
   }
 }
 
-bool read_ultrasonic()
+/**
+ * Sends out and takes in a pulse from the ultrasonic sensor and measures the 
+ * timing between, then converts it to distance measured. The distance measured
+ * is used to check if the robot is too close or not, represented as a bool type.
+ * Return value determines whether the robot is nudged or not.
+ *
+ * @param[in] TIMEOUT Definition for pulseIn time limit.
+ * @param[in,out] ULTRASONIC Ultrasonic Sensor's pin, written to HIGH or LOW 
+ *                           in OUTPUT mode. Reads in pulse when in INPUT mode.
+ * @return Returns TRUE if distance measured is less than min. distance (too close)
+ *         and FALSE if more than (not too close).
+ */
+double read_ultrasonic()
 {
   pinMode(ULTRASONIC, OUTPUT);
   digitalWrite(ULTRASONIC, LOW);
@@ -96,9 +120,20 @@ bool read_ultrasonic()
   pinMode(ULTRASONIC, INPUT);
   long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
   double distance = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
-  return distance < SAFEDISTANCE && distance > 0;
+  return distance;
 }
 
+/**
+ * Reads in the voltage value from the IR_SENSOR pin and determines if
+ * the robot is too close to the wall based on the measured value in
+ * comparison to the cutoff value. Accounts for baseline IR light from
+ * surroundings. Return value determines if the robot is nudged or not.
+ *
+ * @param[in] IR_SENSOR Pin taking in raw values from IR sensor's V_out.
+ * @param[in] IRCUTOFF Raw reading corresponding to minimum distance from wall.
+ * @return Returns TRUE if measured value is more than cut off (too close) and
+ *         false if it is less (not too close).
+ */
 bool read_IR_sensor()
 {
   decoder(3);
@@ -111,6 +146,20 @@ bool read_IR_sensor()
   else return false;
 }
 
+/**
+ * Makes a single reading of RGB values from the LDR, and iterates over all colours
+ * in coloursArray, computing the difference between each RGB value of each colour
+ * and the RGB values of the reading, normalising the difference with coloursArray[6]
+ * and coloursArray[7], calculating the sum of squared error between the reading
+ * and each colour, and returning the integer code of the colour that yields the
+ * minimum sum of squared errors over the RGB values.
+ *
+ * @param[in] LDR Pin recieving raw values from colour detection circuit.
+ * @param[in] coloursArray array of RGB values corresponding to the 6 colours of 
+ *            paper, and the minimum and maximum RGB values.
+ * @return Returns an integer value corresponding to the paper's colour, 
+ *         according to coloured paper ID list.
+ */
 int detectColour()
 {// Shine each colour, read LDR after some delay
   float readColour[3];
@@ -127,8 +176,8 @@ int detectColour()
     float sumSquareError = 0;
     for(int j = 0; j < 3; j++)
     {
-      float normalisedError = (readColour[j] - coloursArray[i][j]) / (coloursArray[7][j] - coloursArray[6][j]);
-      sumSquareError += normalisedError * normalisedError;
+      float error = readColour[j] - coloursArray[i][j];
+      sumSquareError += error * error;
     }
     if (sumSquareError < smallestError)
     {
@@ -198,10 +247,12 @@ void setup()
 
 void loop()
 {
+  double distance = read_ultrasonic(); //read distance from ultranosic sensor
   if (lineFinder.readSensors() == S1_IN_S2_IN) 
   {
     stopMotor();
     int colour = detectColour();
+    //steering instructions for robot based on colour
     if (colour == RED) turn_left();
     else if (colour == GREEN) turn_right();
     else if (colour == ORANGE) turn_around();
@@ -226,7 +277,7 @@ void loop()
       delay(5000);
     }
   }
-  else if (read_ultrasonic()) nudge_right();
+  else if (distance < SAFEDISTANCE && distance > 0) nudge_right();
   else if (read_IR_sensor()) nudge_left();
   else moveForward();
 }
